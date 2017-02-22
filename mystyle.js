3e7574b1a6256c50907111353046c862e3cad573
manojgunakala <saimanoj.gunakala@gmail.com>
2017-02-21 19:52:44 -0500

    Add files via upload

---
+++ mystyle.js
@@ -0,0 +1,10 @@
+
+function myFunction() { 
+    document.getElementById("Aboutme").innerHTML = "I am from a guy from south India.Becoming a Pilot was my dream when i was child, then by the time I finished my under grad my interest towards information systems were at peaks. Now to complete my interests I am doing masters in Information systems Management.";
+}
+
+
+
+
+
+
