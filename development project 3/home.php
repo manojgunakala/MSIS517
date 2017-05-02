502cc7ab0062ff5f3d15306bbf9d68d3b4e6d77a
manojgunakala <saimanoj.gunakala@gmail.com>
2017-05-02 16:41:20 -0400

    Add files via upload

---
+++ development project 3/home.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+//connect to database
+$db=mysqli_connect("localhost","root","","authentication");
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>devProject3</title>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div class="header">
+    <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
+</div>
+<?php
+    if(isset($_SESSION['message']))
+    {
+         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+         unset($_SESSION['message']);
+    }
+?>
+<div>
+    <h1>Thankyou <?php echo $_SESSION['username']; ?>, for registering with us !</h1></div>
+</div>
+<a href="logout.php">Log Out</a>
+</body>
+</html>
---
+++ development project 3/login.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+//connect to database
+$db=mysqli_connect("localhost","root","","authentication");
+if(isset($_POST['login_btn']))
+{
+    $username=mysql_real_escape_string($_POST['username']);
+    $password=mysql_real_escape_string($_POST['password']);
+    $password=md5($password);
+    $sql="SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result=mysqli_query($db,$sql);
+    if(mysqli_num_rows($result)==1)
+    {
+        $_SESSION['message']="You are now Loggged In";
+        $_SESSION['username']=$username;
+        header("location:back.php");
+    }
+   else
+   {
+                $_SESSION['message']="Username and Password combiation incorrect";
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>devProject3</title>
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+<div class="header">
+    <h1>Login with your Registered Account !</h1>
+</div>
+<?php
+    if(isset($_SESSION['message']))
+    {
+         echo "<div id='error_msg'>".$_SESSION['message']."</div>";
+         unset($_SESSION['message']);
+    }
+?>
+<form method="post" action="login.php">
+  <table>
+     <tr>
+           <td>Username : </td>
+           <td><input type="text" name="username" class="textInput"></td>
+     </tr>
+      <tr>
+           <td>Password : </td>
+           <td><input type="password" name="password" class="textInput"></td>
+     </tr>
+      <tr>
+           <td></td>
+           <td><input type="submit" name="login_btn" class="Log In"></td>
+     </tr>
+
+</table>
+</form>
+</body>
+</html>
